e5e46cb942fb2691d94f1bac0473697f2b1e8738
patraden <denis.patrakhin@gmail.com>
2020-04-22 18:30:27 +0100

    new commit

--- c_base.py
+++ c_base.py
@@ -19,7 +19,6 @@ class c_base:
 		self.i_base=len(tmp_alphabet)
 		self.i_alphabet=tmp_alphabet
 		self.message='Seems user provided base='+str(self.i_base)+'and alphabet='+''.join(self.i_alphabet)
-
 	def __init__(self,n=10):
 		if type(n)==int:
 			if n not in range(2,37):
@@ -27,14 +26,15 @@ class c_base:
 				self.i_base=0
 				self.message='base value is out of [2,36] range for pre-defined alphabets'
 			else:
-				if	n<=10:
+				if n<=10:
 					self.i_alphabet=list(map(chr,[*range(ord('0'),ord('0')+n)]))
 				else:
 					self.i_alphabet=list(map(chr,[*range(ord('0'),ord('0')+10),*range(ord('a'),ord('a')+n-10)]))
 					self.i_base=n
 					self.message='base='+str(self.i_base)+'alphabet='+''.join(self.i_alphabet)
-				else:
-					try:
-						self.__user_input_handle__(n)
-					except TypeError:
-						self.__user_input_handle__(str(n))
+		else:
+			try:
+				self.__user_input_handle__(n)
+			except TypeError:
+				self.__user_input_handle__(s)
+print ('Hello, World!')
