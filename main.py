7c1682e6361cfdf334c9128d31fd7832c6add274
patraden <denis.patrakhin@gmail.com>
2021-01-26 09:22:02 +0000

    new commit

--- main.py
+++ main.py
@@ -45,42 +45,42 @@ class bint(int):
     def _add(self, other):
         """
         Auxilary addition method.
-        Algorythm mimics half-adder circuit.
+        Algorythm mimics half-adder circuit of CPU.
         Algorythm relies on assumption that python always interprets integers as 2’s compliment with binarywise operations.
-        s var represents sum output.
-        c var represents carry output.
-        bitmask var is required to handle "travellingi bit" situations.
-        "travelling bit" situation happens in main algorythm when carry has single bit remaining in certain position.
-        While s left remainder (to the same position) has all bits (s is negative).
-        in this case remaining bit in carry keeps travelling left through alrorythm and loop does not exit.
+        "s" var represents sum output.
+        "c" var represents carry output.
+        "bitmask" var is required to handle "travellingi bit" situations.
+        "travelling bit" situation happens in main algorythm when "c" has single bit remaining in certain position.
+        While "s" left remainder (to the same position) has only bits ("s" is negative).
+        In this case remaining bit in "c" keeps travelling left through alrorythm and loop does not exit.
         """
 
         max_bit_pos = max(self.bit_length(), other.bit_length()) + 1
         bitmask = -1 << max_bit_pos
         _bitmask = ~bitmask
 
-        # main algorythm for half-adder circuit
+        # Main algorythm for half-adder circuit
         c,s = (self & other) << 1, self ^ other
         while c and c & _bitmask:
             c,s = (s & c) << 1, s ^ c
 
-        # exception check for "travelling" bit in carry variable
+        # Exception check for "travelling bit"
         if c & bitmask:
 
             c = c >> max_bit_pos
             shifts = 0
-            # finding remaining bit position and capture count of shifts
+            # Finding remaining bit position and capture count of shifts
             while c != 1:
                 c = c >> 1
                 shifts += 1
 
-            # adjusting bitmask to zero left bits remainder of sum
+            # Adjusting bitmask to zero left bits remainder of sum
             bitmask = bitmask << shifts
-            # esnure left bits remainder in sum has expected structure
+            # Esnure left bits remainder in sum has expected structure
             if s >> (max_bit_pos + shifts) == -1:
                 s = s ^ bitmask
             else:
-            # in theory any different sum remainder structure is not possible
+            # In theory any different sum remainder structure is not possible
                 raise Exception ('Impossible case!')
         return bint(s)
 
@@ -105,8 +105,8 @@ if __name__ == "__main__":
     t1 = time.perf_counter()
     print("Time elapsed: ", t1 - t0) # CPU seconds elapsed (floating point)
 
-    a = 3721687328139138019310370127321037120371203710720371203710739021792103710923710293792173901730913709209319371377472645653652364562546546532764527542754754265454272387627816372637136821763187361287361278361873617836872163827163781236817263821736217361783612876816312132786318631863123131231231313123131313213131312313131313123178
-    b = -321691372813321803721072303710730192739273913921730137207310973283183232131312321213213211232131313231321321313123123123132313123131213132331313232213312784273478923748927489374982846296437964289463289648296493264274629846239469264926394629463984628964983264982694862398462986498326489264982364926498236439826983624926946239649286984628946928649826489326498264928364928643928649264982649264928648264982649862946928468246826492431231287897870870
+    a = 3721687328139138019310370199318290839182932811023893812903820913891028301923821903821093812932109371023721073012370123710923710937129037012937012937210973109273712037210937137103710371209371937210937102371209371037012937109273019273091273091730912739012739017309173091730912730917310927317037102372130927321037120371203710720371203710739021792103710923710293792173901730913709209319371377472645653652364562546546532764527542754754265454272387627816372637136821763187361287361278361873617836872163827163781236817263821736217361783612876816312132786318631863123131231231313123131313213131312313131313123178
+    b = -321691372813321803721072303710730192739273913921730137207310973283183232131312321213213211232131313231321321313123123123132313123131213132331313232213312784273478923748927489374982846296437964289463289648296493264274629846239469264926394629463984628964983264982694862398462986498326489264982364926498238918309183728173891738297398218321939269666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666666888888888888888888888888888888888888888888888888888888888888888838983128963921631236439826983624926946239649286984628946928649826489326498264928364928643928649264982649264928648264982649862946928468246826492431231287897870870
     ab=bint(a)
     bb=bint(b)
 
